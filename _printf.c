6c4a10433cf37c717a18cd4f6d6424b769d643c3
redouane <redouane.afoulous@gmail.com>
2023-08-21 23:14:25 -0700

    edited

--- _printf.c
+++ _printf.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * _printf - Custom printf function
- * @format: Format string
- *
- * Description: This function implements some functions of printf.
- * Return: Number of characters printed
+ *_printf - this is print function
+ *@format: const char pointer
+ *Description: this function is executing a set of printf functions
+ *Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	const char *string;
-	int cont = 0;
-	va_list arg;
+	int count = 0;
+	vlist arg;
 
 	if (!format)
 		return (-1);
@@ -19,23 +17,21 @@ int _printf(const char *format, ...)
 	va_start(arg, format);
 	string = format;
 
-	cont = loop_format(arg, string);
+	count = loop_format(arg, string);
 
 	va_end(arg);
-	return (cont);
+	return (count);
 }
-
 /**
- * loop_format - Loop through format string
- * @arg: va_list argument
- * @string: Pointer to format string
- *
- * Description: This function iterates through the format string.
- * Return: Number of characters printed
+ *loop_format - this is loop format
+ *@arg: vlist arg
+ *@string: pointer from format
+ *Description: This is a function that makes a loop string pointer 
+ *Return: number of characters printed
  */
-int loop_format(va_list arg, const char *string)
+int loop_format(vlist arg, const char *string)
 {
-	int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
+	int i = 0, flag = 0, cfm = 0, count = 0, chkper = 0;
 
 	while (i < _strlen((char *)string) && *string != '\0')
 	{
@@ -48,73 +44,68 @@ int loop_format(va_list arg, const char *string)
 			if (aux == '\0' && _strlen((char *)string) == 1)
 				return (-1);
 			if (aux == '\0')
-				return (cont);
+				return (count);
 			if (aux == '%')
 			{
 				flag++;
-			}
-			else
+			} else
 			{
-				cont_fm = function_manager(aux, arg);
-				if (cont_fm >= 0 && cont_fm != -1)
+				cfm = function_manager(aux, arg);
+				if (cfm >= 0 && cfm != -1)
 				{
 					i++;
 					aux = string[i];
 					if (aux == '%')
 						flag--;
-					cont += cont_fm;
-				}
-				else if (cont_fm == -1 && aux != '\n')
+					count = count + cfm;
+				} else if (cfm == -1 && aux != '\n')
 				{
-					cont += _putchar('%');
+					count += _putchar('%');
 				}
 			}
 		}
-		check_per = check_percent(&flag, aux);
-		cont += check_per;
-		if (check_per == 0 && aux != '\0' && aux != '%')
-			cont += _putchar(aux), i++;
-		check_per = 0;
+		chkper = chkpercent(&flag, aux);
+		count += chkper;
+		if (chkper == 0 && aux != '\0' && aux != '%')
+			count += _putchar(aux), i++;
+		chkper = 0;
 	}
-	return (cont);
+	return (count);
 }
-
 /**
- * check_percent - Check and print '%' character
- * @flag: Value by reference
- * @aux: Character
- *
- * Description: This function prints '%' character.
- * Return: 1 if '%' is printed, else 0
+ * chkpercent - call function manager
+ *@flag: value by reference
+ *@aux: character
+ *Description: This function prints % per
+ *Return: 1 if % is printed
  */
-int check_percent(int *flag, char aux)
+int chkpercent(int *flag, char aux)
 {
 	int tmp_flag;
-	int cont = 0;
+	int count = 0;
 
 	tmp_flag = *flag;
 	if (tmp_flag == 2 && aux == '%')
 	{
 		_putchar('%');
 		tmp_flag = 0;
-		cont = 1;
+		count = 1;
 	}
-	return (cont);
+	return (count);
 }
 
 /**
- * call_function_manager - Call function manager
- * @aux: Character parameter
- * @arg: va_list argument
- *
- * Description: This function calls the function manager.
- * Return: Number of characters printed
+ * call_function_manager - this calls the function manager
+ *@aux: character param aux
+ *@arg: vlist arg
+ *Description: This is a function that calls the function manager
+ *Return: number of characters printed
  */
-int call_function_manager(char aux, va_list arg)
+
+int call_function_manager(char aux, vlist arg)
 {
-	int cont = 0;
+	int count = 0;
 
-	cont = function_manager(aux, arg);
-	return (cont);
+	count = function_manager(aux, arg);
+	return (count);
 }
-
